8768b5bfbbe4aed9b982641c51b19f8e59a9e7b3
grandmathebaby <sachamedjo@gmail.com>
2024-09-04 15:34:21 -0700

    9/4

--- MedjoAkonoSacha_PS1.R
+++ MedjoAkonoSacha_PS1.R
@@ -16,25 +16,25 @@ SDBottom
 ##
 VarianceTop<-var(Protozoa$Top, na.rm=TRUE)
 VarianceBottom<-var(Protozoa$Bottom)
-##
+##--
 CITop<-MeanTop-qt(0.95,19)*SDTop
 CIBottom<-MeanBottom+qt(0.95,19)*SDBottom
 CITop
 CIBottom
-##
+##--
 ##-- Question 3
 rm(list=ls())
 Gonad<- read.csv("Data/kelp bass gonad mass.csv")
 View(Gonad)
-##
+##--
 MeanGonad<-mean(Gonad$gonad_mass)
 MedianGonad<-median(Gonad$gonad_mass)
 VarianceGonad<-var(Gonad$gonad_mass)
 SDGonad<-sd(Gonad$gonad_mass)
 CVGonad<-(SDGonad/MeanGonad)
-##
+##--
 ## install.packages("PerformanceAnalytics")
 ## library(PerformanceAnalytics)
-##
+##--
 SkewGonad<-skewness(Gonad$gonad_mass)
 KurtosisGonad<-kurtosis(Gonad$gonad_mass)
