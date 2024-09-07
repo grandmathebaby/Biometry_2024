3542edeb0796383ba0ddc30cbb2f43ce31be5157
grandmathebaby <sachamedjo@gmail.com>
2024-09-07 00:24:54 -0700

    9/6

--- Homeworks/MedjoAkonoSacha_PS1.R
+++ Homeworks/MedjoAkonoSacha_PS1.R
@@ -3,40 +3,26 @@
 rm(list=ls())
 Protozoa<- read.csv("Data/Protozoa.csv")
 View(Protozoa)
-##-- Question 2
-MeanTop<-mean(Protozoa$Top, na.rm=TRUE)
-MeanBottom<-mean(Protozoa$Bottom)
-MeanTop
-MeanBottom 
-##
-SDTop<-sd(Protozoa$Top, na.rm=TRUE)
-SDBottom<-sd(Protozoa$Bottom)
-SDTop
-SDBottom
-##
-VarianceTop<-var(Protozoa$Top, na.rm=TRUE)
-VarianceBottom<-var(Protozoa$Bottom)
+#Getting the stats
+describeBy(Protozoa, group=Protozoa$Sample)
+##--
+Variance<-var(Protozoa, group=Sample, na.rm = TRUE)
+
+VarianceBottom<-var(Protozoa$Density)
 ##--
 CITop<-MeanTop-qt(0.95,19)*SDTop
 CIBottom<-MeanBottom+qt(0.95,19)*SDBottom
-CITop
-CIBottom
+##-- Question 2
 ##load packages
 library(tidyverse)
 library(car)
-##ggplot(Protozoa) + 
-#geom_boxplot(aes(x=Top, y=MeanTop)) + 
-#scale_fill_manual(values=c("#9bc9fa","maroon"), name="Density") +
-#scale_y_log10() + theme_bw() + 
-#labs(y="Mean", x="Density", title="Protozoa population density increases with food available") +
-#theme(legend.position="none")
+##Making my dataset to work with
 
-ggplot(Protozoa, aes(Top, MeanTop)) + 
-  geom_bar(stat = "identity", width=0.8, alpha=0.8) 
-  geom_errorbar(aes(ymin=MeanTop-SDTop, ymax=MeanTop+SDTop), width=0.2) 
-  
- 
 
+ggplot(Protozoa, aes(MeanTop, MeanTop)) + 
+  geom_bar(stat = "identity", width=0.8, alpha=0.8) + 
+  geom_col(position=dodge) +
+  geom_errorbar(aes(ymin=MeanTop-SDTop, ymax=MeanTop+SDTop), width=0.2)
 
 ##--???
 ##-- Question 3
