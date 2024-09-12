ce635fed84f9a66ac410695bb3dbcaed5323e8d2
grandmathebaby <sachamedjo@gmail.com>
2024-09-12 14:00:18 -0700

    9/12

--- Homeworks/PS2/MedjoS_ProblemSet2.R
+++ Homeworks/PS2/MedjoS_ProblemSet2.R
@@ -1,11 +1,19 @@
 #--Sacha Medjo-Akono
 #--Problem Set 2 - Fall 2024
-
 rm(list=ls())
-Visitors <- 
-
+urchins <- read_csv("Homeworks/PS2/Urchins.csv")
+View(urchins)
 ##load packages
 library(tidyverse)
 library(car)
 library(psych)
 library(moments)
+##--1
+flatmean <- mean(urchins$Flat)
+complexmean <- mean(urchins$Complex)
+##--
+Varflat<-var(urchins$Flat)
+Varcomplex<-var(urchins$Complex)
+##--
+SDflat<-sqrt(Varflat)
+SDcomplex<-sqrt(Varcomplex)
