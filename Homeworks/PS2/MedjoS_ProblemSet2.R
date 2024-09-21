376d3dabb9e912b004a00dd9d71bf9a3ce76479f
grandmathebaby <sachamedjo@gmail.com>
2024-09-20 22:03:50 -0700

    9/20

--- Homeworks/PS2/MedjoS_ProblemSet2.R
+++ Homeworks/PS2/MedjoS_ProblemSet2.R
@@ -8,23 +8,41 @@ library(tidyverse)
 library(car)
 library(psych)
 library(moments)
-##--1
+##--1a
 UFlat <- urchins  %>%
   filter(Location == "Flat")
-UFlat 
 UComplex <- urchins %>%
   filter(Location == "Complex")
-
-flatmean <- mean(urchins$Location)  %>%
-  filter(Location == "Flat")
-
-complexmean <- mean(urchins$Complex)
+##
+flatmean <- mean(UFlat$Density)
+complexmean <- mean(UComplex$Density)
 ##--
-Varflat<-var(urchins$Flat)
-Varcomplex<-var(urchins$Complex)
+Varflat<-var(UFlat$Density)
+Varcomplex<-var(UComplex$Density)
 ##--
 SDflat<-sqrt(Varflat)
 SDcomplex<-sqrt(Varcomplex)
+##--1b
+UPooled <- t.test(Density~Location, var.equal=TRUE, data=urchins)
+UPooled
+##
+USep <- t.test(Density~Location, data=urchins)
+USep
+##--1c
+ggplot(urchins, aes(y=Density, x=Location, fill=Location)) + 
+  geom_boxplot(alpha=0.5) +
+  geom_jitter(width=0.2) + 
+  labs(
+    title = "Increased Urchin Density on Rocks with Complex Topography",
+    x = "Rock Topography",
+    y = "Urchin Density"
+  ) +
+  theme_minimal() +
+  scale_fill_manual(values = c("maroon", "dodgerblue")) +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+  )
 ##--2
-mytest1 <- t.test(urchins~Location, var.equal=TRUE, data=urchins, na.rm=TRUE)
-mytest1
+rm(list=ls())
+Wheat <- read_csv("Homeworks/PS2/CoastBuckwheat.csv")
+View(Wheat)
