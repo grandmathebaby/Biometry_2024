e19e77371058324c7ace0b9d9ddab20f7fd8bdb4
grandmathebaby <sachamedjo@gmail.com>
2024-09-25 17:22:04 -0700

    9/25 afternoon

--- Homeworks/PS2/MedjoS_ProblemSet2.R
+++ Homeworks/PS2/MedjoS_ProblemSet2.R
@@ -9,7 +9,7 @@ library(car)
 library(psych)
 library(moments)
 library(MoMAColors)
-##--1a
+##--1
 UFlat <- urchins  %>%
   filter(Location == "Flat")
 UComplex <- urchins %>%
@@ -29,7 +29,7 @@ UPooled
 ##
 USep <- t.test(Density~Location, data=urchins)
 USep
-##--1c
+##
 ggplot(urchins, aes(y=Density, x=Location, fill=Location)) + 
   geom_boxplot(alpha=0.5) +
   geom_jitter(width=0.2) + 
@@ -43,6 +43,7 @@ ggplot(urchins, aes(y=Density, x=Location, fill=Location)) +
   theme(
     plot.title = element_text(hjust = 0.5),
   )
+
 ##--2
 rm(list=ls())
 Wheat <- read_csv("Homeworks/PS2/CoastBuckwheat.csv")
@@ -65,7 +66,6 @@ highCI
 ##-- If I wanted to do it "by hand"
 #lowCI is mean(Wheat$Density)-(qt(0.975,29)*(sd(Wheat$Density)/sqrt(30)))
 #highCI is mean(Wheat$Density)+(qt(0.975,29)*(sd(Wheat$Density)/sqrt(30)))
-
 ##--Plotting
 ggplot(data.frame(x=Wheat$Density), aes(x=x)) +
   geom_histogram(fill="lightblue", color="black", bins=10) +
@@ -75,7 +75,6 @@ ggplot(data.frame(x=Wheat$Density), aes(x=x)) +
   geom_vline(aes(xintercept = highCI, color="Upper CI 5.77")) +
   geom_hline(aes(yintercept = 4, color="Standard Mean (4 plants/25m^2)")) +
   guides(color = guide_legend(title = "Legend"))
-
 ##-- Bootstrapping
 WheatBoots <- replicate(1000, {
   samples <- sample(Wheat$Density,replace=TRUE);
@@ -95,6 +94,7 @@ ggplot(data.frame(x=SortedWB), aes(x=x)) +
   geom_vline(aes(xintercept = HighBoot, color="Upper CI")) +
   geom_hline(aes(yintercept = 4, color="Standard Mean (4 plants/25m^2)")) +
   guides(color = guide_legend(title = "Legend"))
+
 ##--3
 rm(list=ls())
 RT <- read_csv("Homeworks/PS2/RunTimes.csv")
@@ -105,6 +105,7 @@ var.test(RT$Sportsdrink,RT$Water)
 #
 RTtest <- t.test(RT$Water, RT$Sportsdrink, paired=TRUE, data=RT)
 RTtest
+
 ##--4
 rm(list=ls())
 crybbs <- read_csv("Homeworks/PS2/crying_babies.csv")
@@ -136,6 +137,7 @@ Spearmansbbys
 ##--Kendall's tau
 Kendallsbbys <- cor.test(Ranks$cryrank, Ranks$iqrank, method = "kendall")
 Kendallsbbys
+
 ##--5
 rm(list=ls())
 Floridaman <- read_csv("Homeworks/PS2/butterflyballot.csv")
@@ -145,17 +147,15 @@ NoPalm<- Floridaman[-67,]
 glimpse(NoPalm)
 shapiro.test(NoPalm$Bush)
 shapiro.test(NoPalm$Buchanan)
-##--
-FloRanks <- NoPalm %>% mutate(Bushrank = min_rank(Bush), Buchrank = min_rank(Buchanan))
-glimpse(FloRanks)
+##-Without Palm
 ##--Pearsons r
-Pearsons <- cor.test(FloRanks$Bush,FloRanks$Buchanan, method = "pearson")
+Pearsons <- cor.test(NoPalm$Bush,NoPalm$Buchanan, method = "pearson")
 Pearsons
 #--Spearman's rho
-Spearmans <- cor.test(FloRanks$Bushrank,FloRanks$Buchrank, method = "spearman")
+Spearmans <- cor.test(NoPalm$Bush,NoPalm$Buchanan, method = "spearman")
 Spearmans
 ##--Kendall's tau
-Kendalls <- cor.test(FloRanks$Bushrank,FloRanks$Buchrank, method = "kendall")
+Kendalls <- cor.test(NoPalm$Bush,NoPalm$Buchanan, method = "kendall")
 Kendalls
 ##-- With Palm
 ##--Pearsons r
@@ -167,3 +167,17 @@ Spearmans2
 ##--Kendall's tau
 Kendalls2 <- cor.test(Floridaman$Bush,Floridaman$Buchanan, method = "kendall")
 Kendalls2
+
+##--
+rm(list=ls())
+Streams <- read_csv("Homeworks/PS2/Streams.csv")
+view(Streams)
+##
+##--Plot
+ggplot(Streams, aes(x=NumberSpp, y=Biomass)) +
+  geom_point(size=2) +
+  geom_smooth(method="lm", se=)
+  scale_fill_manual(values=c(moma.colors("Connors", 3)), name="Biomass") + 
+  labs(x="Streams", y="Biomass", title="the biomass is changing") +
+  theme(legend.position="bottom") +
+  guides(color = guide_legend(title = "Legend"))
