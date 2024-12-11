33ed83435f2bc3a687e19f9b8592218d6443ce1d
grandmathebaby <sachamedjo@gmail.com>
2024-12-11 12:24:14 -0800

    12/11 morning

--- Homeworks/FINAL/MedjoS_Final.R
+++ Homeworks/FINAL/MedjoS_Final.R
@@ -31,3 +31,24 @@ qqp(frogres, "norm")
 #
 plot(frogres~fitted(frogmod)) #not a cone :)
 summary(frogmod)
+#--Question 2
+#There are 214 dead animals
+rm(list=ls())
+rural <- 119
+urban <- 95
+#
+observed <- matrix(c(rural, urban), nrow=1, ncol=2)
+expected <- matrix(c((rural+urban)/2, (rural+urban)/2), nrow=1, ncol=2)
+#G Value
+Gvalue <- 2*sum(observed*log(observed/expected))
+Gvalue #is 2.69726
+1-pchisq(Gvalue, df=1)
+#p-value = 0.1005209
+#Chi-squared test
+X2test <- sum((observed-expected)^2/expected)
+X2test # is 2.691589
+1-pchisq(X2test, df=1) #p-value = 0.01008792
+#--Question 3
+
+
+
