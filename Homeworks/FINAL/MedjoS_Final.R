dd54df574aab1a278b634d1b99ed152fcf3938ae
grandmathebaby <sachamedjo@gmail.com>
2024-12-13 18:07:28 -0800

    12/13 i know what u did

--- Homeworks/FINAL/MedjoS_Final.R
+++ Homeworks/FINAL/MedjoS_Final.R
@@ -10,7 +10,9 @@ library(pscl)
 library(factoextra)
 library("pwr")
 library(MASS)
+library(ggsignif)
 library(MoMAColors)
+library(lme4)
 display.all.moma()
 
 #--Question 1
@@ -84,11 +86,11 @@ ggplot(ana, aes(y=WeightChange, x=Treatment, fill=Treatment)) +
   theme(
     plot.title = element_text(hjust = 0.5)) +
   geom_signif(data=ana,stat="signif",position="identity",
-              comparisons=list(c("Control","Therapy")),map_signif_level = TRUE,annotations="***")
+              comparisons=list(c("Control","Therapy")),map_signif_level = TRUE,annotations="*")
 #--Question 4
 rm(list=ls())
 Bass <- read_csv("Homeworks/FINAL/habitats.csv")
-View(Bass)
+glimpse(Bass)
 #
 Bass1 <- Bass[,-c(1,2,3)] #Only 6 habitat traits
 bass.scale <- scale(Bass1, scale=TRUE, center=TRUE) #convert to z-scores
@@ -99,3 +101,90 @@ summary(PCAmodel)
 plot(PCAmodel, type="lines")
 library(factoextra)
 fviz_eig(PCAmodel)
+#
+PCAmodel$loadings  # Shows the loadings of each trait on each PC 
+#perMANOVA
+library("vegan")
+library(devtools)
+install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
+library(pairwiseAdonis)
+#
+perm <- adonis2(Bass1~Site, Bass, 
+                          permutations = 999, method="bray")
+perm
+perm2 <- adonis(Bass1~Site, Bass, 
+                          permutations = 999, method="bray")
+summary(perm2)
+perm2$coefficients
+#
+disper <- vegdist(Bass1)
+betadisper(disper, Bass$Site)#oknnonumber twice as big
+#Multiple Regression
+bassmod <- lm(KelpBassDensity~KelpVolume + Temperature + Salinity + Sand +
+                Seagrass + Rock, data=Bass)
+library(mctest)
+imcdiag(bassmod) #No collinearty :)
+AIC(bassmod)
+AIC(basslogmod) #Best Model
+plot(bassmod)
+#
+anova(bassmod)
+#
+predicted <- fitted(bassmod)
+resid <- residuals(bassmod)
+plot(resid~predicted)
+abline(h=0) # a bit coney...
+#
+library(car)
+qqp(resid, "norm") #meets normality
+#Logged model to see about that cone
+basslog <- log((Bass$KelpBassDensity) + 1)
+basslogmod <- lm(basslog ~ Temperature + Salinity + KelpVolume + Sand +
+                   Seagrass + Rock, data=Bass)
+plot(basslogmod)
+
+predicted2 <- fitted(basslogmod)
+resid2 <- residuals(basslogmod)
+
+anova(basslogmod)
+anova(bassmod)
+#--Question 5
+rm(list=ls())
+dillo <- read_csv("Homeworks/FINAL/armadillos.csv")
+glimpse(dillo)
+#Logistic Regression
+ggplot(dillo, aes(x=Burrows)) + geom_histogram(fill="seagreen") + 
+  labs(x="Dillos present?", y="Grain Size") +
+  theme_bw()
+#
+dillo1 <- glm(Burrows ~ GrainSize, family = binomial(link="logit"), data=dillo)
+Anova(dillo1) # Yes sig p<0.05
+library(pscl)
+pR2(dillo1) #McFadden r2 0.1253636 
+#Plot
+plot(dillo$GrainSize, dillo$Burrows, xlab="Soil Grain Size",ylab="Probability of Armadillo Burrow")
+curve(predict(dillo1,data.frame(GrainSize=x),type="resp"),add=TRUE)
+#Beautiful Plot
+library(ggplot2)
+library(dplyr)
+to.predict <- data.frame(GrainSize=seq(0.24,1.05,0.01))
+predicted.data <- cbind(to.predict, as.data.frame(predict(dillo1, newdata=to.predict, type="response", se=TRUE))) 
+glimpse(predicted.data) 
+#
+ggplot(dillo, aes(x=GrainSize, y=Burrows)) +
+  geom_point() +
+  geom_line(data = predicted.data, aes(x=GrainSize, y=fit), color="palevioletred") +
+  labs(x="Soil Grain Size", y="Presence of Armadillo Burrow",
+       title="Armadillos Favor Larger Soil Grain Size for Burrowing") +
+  theme_minimal()
+#
+to.predict <- data.frame(PA_ratio=seq(5,65,5))
+predicted.data <- cbind(to.predict, as.data.frame(predict(model1, newdata=to.predict, type="response", se=TRUE)))
+
+glimpse(predicted.data)
+
+ggplot(lizards, aes(x=PA_ratio, y=Present)) +
+  geom_point() +
+  geom_line(data = predicted.data, aes(x=PA_ratio, y=fit), color="blue") +
+  labs(x="Perimeter:Area Ratio", y="Presence of Lizards") +
+  theme_bw() 
